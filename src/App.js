2c9d73710062007675f06660fe962dd2f7c788f0
Anna <anutasash@mail.ru>
2024-02-14 13:38:34 +0300

    task

--- src/App.js
+++ src/App.js
@@ -2,24 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const userName = 'Anna';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {userName}</h1>
+      {/* <Heading />
+      <Heading />
+      <NewComp /> */}
+      <Article number="3" name="Info" />
+      <Article number="5" name="Blog" />
     </div>
   );
 }
+
+function Article({ name, number }) {
+  let themeSiteWhiite = true;
+  return (
+    <div>
+      <h2 style={{ color: themeSiteWhiite ? 'green' : 'red' }} className='heading'>Статья про {name} номер{number}</h2>
+      <a href="#">Читать статью.</a>
+    </div>
+  );
+}
+
+// function NewComp() {
+//   return (
+//     <div>New text</div>
+//   );
+// }
+
+// function Heading() {
+//   return (
+//     <div>  <h2>новый текст внутри заголовка</h2>
+//       <p>новый текст</p>
+//       <a href="#">читать далее...</a>
+//     </div>
+//   );
+// }
 
 export default App;
